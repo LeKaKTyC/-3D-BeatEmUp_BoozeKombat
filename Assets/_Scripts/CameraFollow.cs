b270ff4aa0d6ccd89b99f9d5759d3801b31b4fe4
Vik Lul <mr47078@gmail.com>
2024-10-16 14:44:18 +0300

    New Script added
    CameraFollow.cs

---
+++ Assets/_Scripts/CameraFollow.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Vector3 MiddlePosition;
+
+    [Header("Player Targets")]
+    public GameObject targetPlayer;
+
+    [Header("Follow Settings")]
+    public float distanceToTarget = 10; // The distance to the target
+    public float heightOffset = -2; // the height offset of the camera relative to it's target
+    public float viewAngle = -6; //a downwards rotation
+    public Vector3 AdditionalOffset; //any additional offset
+    public bool FollowZAxis; //enable or disable the camera following the z axis
+
+    [Header("Damp Settings")]
+    public float DampX = 3f;
+    public float DampY = 2f;
+    public float DampZ = 3f;
+
+    [Header("View Area")]
+    public float MinLeft;
+    public float MaxRight;
+
+    [Header("Wave Area collider")]
+    public bool UseWaveAreaCollider;
+    public BoxCollider CurrentAreaCollider;
+    public float AreaColliderViewOffset;
+    private bool firstFrameActive;
+
+    //updates the targets to follow
+    public void UpdatePlayerTarget()
+    {
+        targetPlayer = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Start()
+    {
+        UpdatePlayerTarget();
+        firstFrameActive = true;
+    }
+
+    void FixedUpdate()
+    {
+ 
+            if (targetPlayer != null) MiddlePosition = targetPlayer.transform.position;
+
+            //initial values
+            float currentX = transform.position.x;
+            float currentY = transform.position.y;
+            float currentZ = transform.position.z;
+
+            //Damp X
+            currentX = Mathf.Lerp(currentX, MiddlePosition.x, DampX * Time.fixedDeltaTime);
+
+            //DampY
+            currentY = Mathf.Lerp(currentY, MiddlePosition.y - heightOffset, DampY * Time.fixedDeltaTime);
+
+            //DampZ
+            if (FollowZAxis)
+            {
+                currentZ = Mathf.Lerp(currentZ, MiddlePosition.z + distanceToTarget, DampZ * Time.fixedDeltaTime);
+            }
+            else
+            {
+                currentZ = distanceToTarget;
+            }
+
+            //set values for 1st frame (No damping)
+            if (firstFrameActive)
+            {
+                firstFrameActive = false;
+                currentX = MiddlePosition.x;
+                currentY = MiddlePosition.y - heightOffset;
+                currentZ = FollowZAxis ? (MiddlePosition.z + distanceToTarget) : distanceToTarget;
+            }
+
+            //Set cam position
+            if (CurrentAreaCollider == null) UseWaveAreaCollider = false;
+            if (!UseWaveAreaCollider)
+            {
+                transform.position = new Vector3(Mathf.Clamp(currentX, MaxRight, MinLeft), currentY, currentZ) + AdditionalOffset;
+            }
+            else
+            {
+                transform.position = new Vector3(Mathf.Clamp(currentX, CurrentAreaCollider.transform.position.x + AreaColliderViewOffset, MinLeft), currentY, currentZ) + AdditionalOffset;
+            }
+
+            //Set cam rotation
+            transform.rotation = new Quaternion(0, 0, 0, 0);
+        }
+    }
+
+    
+
