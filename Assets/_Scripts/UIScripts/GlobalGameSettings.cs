792a447bd6b1be3082bb43a7b9cbe3ab93b90300
Vik Lul <mr47078@gmail.com>
2024-10-24 20:15:32 +0300

    UI template added to a scene
    UI obj is set on a  Level 1 scene
    few Prefabs reworked
    project folders cleanup

---
+++ Assets/_Scripts/UIScripts/GlobalGameSettings.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public static class GlobalGameSettings
+{
+    public static GameObject Player1Prefab;
+    public static List<LevelData> LevelData = new List<global::LevelData>();
+    public static int currentLevelId = 0;
+}
